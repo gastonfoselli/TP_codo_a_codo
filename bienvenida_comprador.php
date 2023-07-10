ba0b9c204bd5696ea6562e70a57dbf4f9c0602fa
bendatabi <109187589+bendatabi@users.noreply.github.com>
2023-07-10 10:08:05 -0300

    Add files via upload

---
+++ bienvenida_comprador.php
@@ -0,0 +1,33 @@
+<?php
+
+    session_start();
+
+    if(!isset($_SESSION['usuario'])){
+        echo '
+            <script>
+                alert("Por favor debes iniciar sesión");
+            </script>
+        ';
+        session_destroy();
+        die();
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenida Comprador</title>
+    <link rel="stylesheet" href="css/bienvenida.css">
+</head>
+<body>
+    <h1>Bienvenid@ a TechDreams!</h1>
+    <div>
+        <p>Haz click <a href="pagina_inicio.html">aquí </a>para ir a la tienda virtual</p>
+        <a href="php/cerrar_sesion.php">Cerrar sesión</a>
+    </div>   
+    
+</body>
+</html>
